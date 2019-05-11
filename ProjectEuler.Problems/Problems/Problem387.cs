18902f23fcc3f07b301ab893a6c945f75f8f103c
Mike Conners <mfconners@gmail.com>
2019-05-11 10:13:08 -0500

    Adding Problem 387...

---
+++ ProjectEuler.Problems/Problems/Problem387.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ProjectEuler.Problems
+{
+	class Problem387 : Problem
+	{
+		// This is a template problem...  Copy it and rename it to ProblemXXX, where XXX is the Project Euler problem number.
+		//public override string CorrectAnswer { get { return "???"; } }
+
+		protected override string CalculateSolution()
+		{
+			for (int i = 0;i<1000;++i)
+			{
+				i += i;
+			}
+
+			return "Yay!";
+		}
+	}
+}
