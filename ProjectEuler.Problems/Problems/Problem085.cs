f3d8cd5f077dc093167ee50a4d13f587762a45eb
Mike Conners <mfconners@gmail.com>
2019-05-11 11:51:32 -0500

    Starting 85...

--- ProjectEuler.Problems/Problems/Problem085.cs
+++ ProjectEuler.Problems/Problems/Problem085.cs
@@ -3,9 +3,8 @@ using System.IO;
 
 namespace ProjectEuler.Problems
 {
-	class Problem000 : Problem
+	class Problem085 : Problem
 	{
-		// This is a template problem...  Copy it and rename it to ProblemXXX, where XXX is the Project Euler problem number.
 		//public override string CorrectAnswer { get { return "???"; } }
 
 		protected override string CalculateSolution()
